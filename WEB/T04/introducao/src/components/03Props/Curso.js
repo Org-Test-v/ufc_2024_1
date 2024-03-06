83d2ac650a39f7db4303e693d9cf5d0b6918cc32
gabrielraulino <gabrielviana@alu.ufc.br>
2024-03-06 17:22:38 -0300

    resolucao props

--- WEB/T04/introducao/src/components/03Props/Curso.js
+++ WEB/T04/introducao/src/components/03Props/Curso.js
@@ -1,1 +1,14 @@
 //imprima o nome do curso e pelo menos 3 disciplinas
+//imprima o nome do curso e pelo menos 3 disciplinas
+import Disciplina from "./Disciplina";
+const Curso = () => {
+  return(
+    <div>
+      <h1>Cursos UFC Quixadá</h1>
+      <hr/>
+      <Disciplina nome="Computação em nuvem" cod="35" credito={4} periodo="Vespertino"></Disciplina>
+    </div>
+  )
+}
+
+export default Curso
--- WEB/T04/introducao/src/components/03Props/Disciplina.js
+++ WEB/T04/introducao/src/components/03Props/Disciplina.js
@@ -1,1 +1,23 @@
 //nome, código da disciplina, créditos, período
+const Disciplina = ({nome, cod, credito, periodo}) => {
+
+  return(
+    <div>
+      <h3>
+        Nome da disciplina: {nome}
+      </h3>
+      <h3>
+        Código da disciplina: {cod}
+      </h3>
+      <h3>
+        Quantidade de créditos: {credito}
+      </h3>
+      <h3>
+        Período da disciplina: {periodo}
+      </h3>
+    </div>
+  )
+  
+}
+
+export default Disciplina
