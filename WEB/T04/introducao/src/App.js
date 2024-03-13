92198739c498aae550712e02118bdd1fecbed5b8
gabrielraulino <gabrielviana@alu.ufc.br>
2024-03-13 11:34:46 -0300

    feat: App limpo

--- WEB/T04/introducao/src/App.js
+++ WEB/T04/introducao/src/App.js
@@ -1,37 +1,9 @@
-<<<<<<< HEAD
-//Utilizando estados
-// utilizando Pokemon API para usar estados
-import Pokemon from "./components/06Estados/Pokemon";
-function App() {
-  return (
+const App = () => {
+  return(
     <div>
-      <h1> Poke API</h1>
-      <Pokemon />
-=======
-//import HelloWorld from "./components/01HelloWorld/HelloWorld";
-//import FuncoesInternas from "./components/02FuncoesInternas/FuncoesInternas";
-//import Loja from "./components/03Props/Loja";
-//import {MeusDados as MD, MeusDadosV1 as MDV1, MeusDadosV2, MeusDadosV3} from "./components/00Atividades/atividade00/01MeusDados";
-//import MeusDadosProps from "./components/00Atividades/atividade00/02MeusDados";
-//import Temperatura from "./components/00Atividades/atividade00/03Temperatiura";
-//import SistemaSolar from "./components/04Children/SistemaSolar";
-//import Planeta from "./components/04Children/Planeta";
-//import Galaxia from "./components/04Children/Galaxia";
-//import ComPai from "./components/05ComPaiFilho/ComPai";
-//import Estado from "./components/06Estados/Estado";
-//import Pokemon from "./components/06Estados/Pokemon";
-//import { CompA } from "./components/07PropDrilling/PropDrilling";
-//import { CompA } from "./components/08Contexto/Contexto";
-import PokemonPrincipal from "./components/09PokemonContexto/PokemonPrincipal";
-
-function App() {
-  return (
-    <div>
-      <h1>Desenvolvimento de Software para a WEB!</h1>
-      <PokemonPrincipal />
->>>>>>> 909f4df85c9a98ebe6d0df28b4be97ada284758b
+      <h1>Olá, mundo!</h1>
     </div>
-  )
+ )
 }
 
 export default App;
